d70a9f4da5f7f2ba5855530c0fe2a5934c751c9e
Ye Hlaing Win <yehlaingwin26203@gmail.com>
2025-09-30 17:39:05 +0800

    Lecture7 In progress

---
+++ Lecture7/Lecture7.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 30 16:58:39 2025
+
+@author: zkkkkk
+"""
+
+# def is_even(i): 
+#     """
+#     """
+#     return i%2 == 0
+
+# print(is_even(3))
+
+
+###############################################################################
+# YOU TRY IT !! 
+###############################################################################
+
+# 1  
+# def div_by(n, d):
+#     """ n and d are ints > 0 
+#         Returns True if d divideds n evenly and False otherwise"""
+#     return n%d == 0
+# print(div_by(10, 3))
+# print(div_by(195, 13))
+
+
+
