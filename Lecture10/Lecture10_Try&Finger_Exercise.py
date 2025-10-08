2068add4ca9a39a7314ed14d47536373181eac94
Ye Hlaing Win <yehlaingwin26203@gmail.com>
2025-10-08 19:01:32 +0800

    lecture 10 Try It! section done

--- Lecture10/Lecture10_Try&Finger_Exercise.py
+++ Lecture10/Lecture10_Try&Finger_Exercise.py
@@ -10,21 +10,21 @@ Created on Wed Oct  8 08:49:23 2025
 ###############################################################################
 
 
-## (1) Write a function that meets the following speciicaion:
+# (1) Write a function that meets the following speciicaion:
     
-# def make_ordered_list(n):
-#     """ n is a positive int
-#     Returns a list containing all ints in order from 0 to n (inclusive).
-#     """
+def make_ordered_list(n):
+    """ n is a positive int
+    Returns a list containing all ints in order from 0 to n (inclusive).
+    """
     
-#     ordered_list = []
+    ordered_list = []
     
-#     for i in range (n+1):
-#         ordered_list.append(i)
+    for i in range (n+1):
+        ordered_list.append(i)
         
-#     return ordered_list
+    return ordered_list
 
-# print(make_ordered_list(6))
+print(make_ordered_list(6))
         
 
 ###############################################################################
@@ -32,22 +32,22 @@ Created on Wed Oct  8 08:49:23 2025
 
 ## (2) Write a function that meets the specification
 
-# def remove_elem(L, e):
-#     """
-#     L is a list
-#     Returns a new list with elements in the same order as L 
-#     but without any elements equal to e.
-#     """
-#     new_list = []
-#     for item in L:
-#         if item != e:
-#             new_list.append(item)
+def remove_elem(L, e):
+    """
+    L is a list
+    Returns a new list with elements in the same order as L 
+    but without any elements equal to e.
+    """
+    new_list = []
+    for item in L:
+        if item != e:
+            new_list.append(item)
         
-#     return new_list;
+    return new_list;
 
   
-# L = [1,2,2,2]
-# print(remove_elem(L, 2))    # prints [1]
+L = [1,2,2,2]
+print(remove_elem(L, 2))    # prints [1]
 # L = [1,2,2,2]
 # print(remove_elem(L, 1))    # prints [2,2,2]
 # L = [1,2,2,2]
@@ -60,17 +60,17 @@ Created on Wed Oct  8 08:49:23 2025
 
 ## (3) Write a function that meets this specification
 
-# def count_words(sen):
-#     """ sen is a string representing a sentence 
-#     Returns how many words are in sen (i.e. a word is a 
-#     a sequence of characters between spaces. """
-#     # your code here+
+def count_words(sen):
+    """ sen is a string representing a sentence 
+    Returns how many words are in sen (i.e. a word is a 
+    a sequence of characters between spaces. """
+    # your code here+
     
-#     words_list = sen.split(" ")
-#     return len(words_list)
+    words_list = sen.split(" ")
+    return len(words_list)
 
-# s = "Hello it's me"
-# print(count_words(s))   # prints 3
+s = "Hello it's me"
+print(count_words(s))   # prints 3
 
 # s = "I just took a DNA test turns out I'm 100% splitting strings"
 # print(count_words(s))   # prints 12
@@ -92,8 +92,8 @@ def sort_words(sen):
     return new_list
     
 
-# s = "look at this photograph"
-# print(sort_words(s))    # prints ['at', 'look', 'photograph', 'this']
+s = "look at this photograph"
+print(sort_words(s))    # prints ['at', 'look', 'photograph', 'this']
 
 # s = "now this is a story all about how my life got flipped turned upside down"
 # print(sort_words(s))
@@ -101,3 +101,63 @@ def sort_words(sen):
 
 
 ###############################################################################
+
+
+## (5) Iteration
+
+##  Option 1: Make a new variable representing the index, initialized to 0
+## before the loop and incremented by 1 in the loop.
+
+def square_list(L):
+    i = 0
+    for elem in L:
+        L[i] = elem**2
+        i += 1
+    return L
+
+
+
+# Option 2: Loop over the index not the element, and use L[index] to get
+# the element
+
+def square_list(L):
+    for i in range(len(L)):
+        L[i] = L[i]**2
+    return L
+
+
+
+
+# Option 3: Use enumerate in the for loop (I leave this option to you to
+# look up). i.e. for i,e in enumerate(L)
+
+def square_list(L):
+    for index, item in enumerate(L):
+        L[index] = item**2
+    return L
+
+
+L = [1,2,3]
+print(square_list(L)) 
+       
+        
+    
+## Option 2 and 3 are recommended and the best
+## Never use option 1 for iteration over a list if we want to mutate the list
+
+
+###############################################################################
+## 
+###############################################################################
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
